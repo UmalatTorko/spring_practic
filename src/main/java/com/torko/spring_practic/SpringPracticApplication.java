67bcc0d0653317befff193dd0d47be9d21d187f5
umalatgordanov <gordanov98@inbox.ru>
2021-02-12 14:31:02 +0300

    Initial commit

---
+++ src/main/java/com/torko/spring_practic/SpringPracticApplication.java
@@ -0,0 +1,13 @@
+package com.torko.spring_practic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torko/spring_practic/SpringPracticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torko.spring_practic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
